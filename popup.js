0ab9a3e0955c9f4bd42dc91b62326a79bf5a879f
ranisharafeeque <ranisharafeeque@gmail.com>
2021-05-19 23:02:46 -0400

    Chrome Extention

---
+++ popup.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('button');
+  console.log(button);
+  document.querySelector('button').addEventListener('click', onClick, false);
+
+  function onClick() {
+    chrome.tabs.query({ currentWindow: true, active: true},
+      function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'Sending message');
+      }
+    )
+  }
+}, false)
